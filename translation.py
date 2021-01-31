bd5e28b9e1663de643375394734f9940e4b2b201
HellBoy-OP <72251918+HellBoy-OP@users.noreply.github.com>
2021-01-31 17:05:13 +0530

    Add files via upload

--- translation.py
+++ translation.py
@@ -1,20 +1,24 @@
 class Translation(object):
-    START_TEXT = """Hi!
-please read the TnC before proceeding: https://t.me/SpEcHlDe/889
-Thank you for using me 😬
-Enter your Telegram Phone Number, to get the APP-ID from my.telegram.org
+    START_TEXT = """👋 Hii! This is Hêllẞø† API ID and API HASH generator. 
 
-/start at any stage to re-enter your details"""
-    AFTER_RECVD_CODE_TEXT = """I see!
-now please send the Telegram code that you received from Telegram!
+⚠️ WE CARE FOR YOUR PRIVACY. SO THIS BOT IS END-TO-END ENCRYPTED. SO NO RISK TO YOUR PRIVACY. **WE ARE NOT FROM FACEBOOK**. SO YOUR DATA IS SAFE ⚠️
 
-this code is only used for the purpose of getting the APP ID from my.telegram.org
-if you do not trust this bot dev, please host this bot yourself
-by opening https://github.com/SpEcHiDe/MyTelegramOrgRoBot and clicking on the Pink Button
+✨ Thank you for using me ✨
 
-/start at any stage to re-enter your details"""
+🔸 Enter your Telegram Phone Number with your country code to get the APP-ID and API HASH.
+
+🔺 Do /start at any stage to re-enter your details
+
+❤️ **Huge Thanks To @SpEcHiDe** ❤️"""
+    AFTER_RECVD_CODE_TEXT = """✅ Ok! Now please send the Telegram code that you received from Telegram!
+
+⚠️ This code is only used for the purpose of getting the APP ID and API HASH from my.telegram.org ⚠️
+
+📌 If you don't trust us you can simply go to my.telegram.org and get your API ID and API HASH ✅
+
+⚡ Do /start at any stage to re-enter your details ⚡"""
     BEFORE_SUCC_LOGIN = "recieved code. Scarpping web page ..."
-    ERRED_PAGE = "something wrongings. failed to get app id. \n\n@SpEcHlDe"
+    ERRED_PAGE = "something wrongings. failed to get app id"
     CANCELLED_MESG = "Bye! Please re /start the bot conversation"
-    IN_VALID_CODE_PVDED = "sorry, but the input does not seem to be a valid Telegram Web-Login code"
-    IN_VALID_PHNO_PVDED = "sorry, but the input does not seem to be a valid phone number"
+    IN_VALID_CODE_PVDED = "Uhh Sorry, but the given input does not seem to be a valid Telegram Web-Login code. You can try re-checking the code or try again!"
+    IN_VALID_PHNO_PVDED = "Uhh Sorry, but the given input does not seem to be a valid phone number.\n\nPossible Errors :- \n✓ Wrong Phone Number\n✓ Country code not provided or maybe wrong."
